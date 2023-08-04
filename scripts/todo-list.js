8ac5dab4acff6cdc98df3ff654940a0d9e1d4de0
Elham Shahrokhi <ellishahi95@gmail.com>
2023-08-04 23:59:37 +0330

    Refactor: replacing addEventListener instead of onclick for delete button

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -1,4 +1,12 @@
-const todoList =[];
+// we should group the name and due date together.
+
+const todoList =[{
+  name: 'make dinner',
+  dueDate: '2023-07-29'
+  },
+  {name: 'wash dishes',
+  dueDate:'2023-07-29'}
+  ];
 //here we use accumulator pattern to make an array to put todo list in there.
 // and because we want to update the todo list every time that we click on add button, we should put the code on a function to reuse that every time.
 
@@ -6,23 +14,49 @@ renderTodoList();
 
 function renderTodoList(){
 let todoListHTML =  '';
-for (let i=0; i< todoList.length; i++){
-  const todo = todoList[i];
+//arrow function
+todoList.forEach((todoObject, index)=>{
+    
+    const {name, dueDate} = todoObject;
+  
+    // the below technique is 'generating the HTML': instead of writing all the HTML by hand up, we looped through any array and we generated the HTML.
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button class="delete-todo-button js-delete-todo-button">Delete</button>`;
+    todoListHTML +=html
 
-  // the below technique is 'generating the HTML': instead of writing all the HTML by hand up, we looped through any array and we generated the HTML.
-  const html = `<p>${todo}</p>`
-  todoListHTML +=html
-  console.log(todoListHTML);
-}
+});
+
 // so this will get that div element and put it inside our JavaScript.
-document.querySelector('.js-todo-list').innerHTML = todoListHTML;}
+document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 
+document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton,index)=>{
+  deleteButton.addEventListener('click', ()=>{
+    todoList.splice(index, 1);
+    renderTodoList();
+  })
+})
+
+}
+
+document.querySelector('.js-add-todo-button').addEventListener('click', ()=> {
+  addTodo();
+})
 
 function addTodo(){
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
-  todoList.push(name);
-  console.log(todoList);
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+  todoList.push({
+  // name:name,
+  // dueDate: dueDate
+  //if the property and the variable name are the same, we can just type a comma and type it out once.
+  name ,
+  dueDate
+  });
+ 
   
 // after adding the value to the array, we want to clear the input box and return to the default.
   inputElement.value = "";
